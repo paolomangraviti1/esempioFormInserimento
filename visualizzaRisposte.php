b4aa3f8309e8cadeed6f11ac0946bccffd6218a1
paolomangraviti1 <paolo.mangraviti1@istruzione.it>
2019-01-29 19:43:16 +0100

    Update visualizzaRisposte.php

--- visualizzaRisposte.php
+++ visualizzaRisposte.php
@@ -1,3 +1,10 @@
+<html>
+    <head>
+            <title>Risposte ottenute</title>
+    </head>
+    <body>
+        <h1>Risposte ottenute</h1>
+        <ul>
 <?php
     /* parametri di connessione al RDBMS */
     $servername = "";   // Indirizzo del server DBMS
@@ -16,10 +23,21 @@
         /* Predisposizione della query SQL */
         $sql = "SELECT * FROM Risposte;')";
         
-        // Invio della query al server DBMS
+        // Invio della query al server DBMS, ottiene il reultSet della query (righe)
         $esitoQuerySQL = $conn->query($sql);
         
-       
+            // Itera tutte le righe del resultSet, ogni riga e' un array i cui elementi sono i campi.
+            // Il valore del campo è possibile ottenerlo:
+            // o specificando la posizione dell'array riga
+            // o specificando la posizione con il nome del campo (dell'array riga)
+           while ($row = $esitoQuerySQL->fetch_assoc())
+           {
+               // echo "<li>" . $row["1"]. "</li>";
+               // echo "<li>" . $row["risposta"]. "</li>";
+           }
     }
     $conn->close();
 ?>
+        </ul>
+    </body>
+</html>
